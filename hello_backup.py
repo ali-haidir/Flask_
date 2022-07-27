a7fc808c5fbab0634abd06524e390483c77c0fb1
Ali Haider <khalihaider8@gmail.com>
2022-07-27 13:41:00 +0500

    “up_untill_authenticating_users_27”

--- hello_backup.py
+++ hello_backup.py
@@ -1,14 +1,14 @@
 from flask import Flask,render_template,flash,request,redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,PasswordField,BooleanField,ValidationError
+from wtforms.validators import DataRequired, EqualTo , Length
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime,date
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask import jsonify
+from wtforms.widgets import TextArea
 from flask_login import UserMixin,login_user,LoginManager,login_required,logout_user,current_user
-
-#referenceing the forms now after shifting them to anouther file
-from webforms import *
-# from webforms import UserForm,NamerForm,PostForm,PasswordForm,LoginForm
 
 
 #create a flask instance
@@ -83,6 +83,45 @@ class Users(db.Model,UserMixin):
     #create String
     def __repr__(self):
         return '<Name %r>' % self.name
+
+
+#Create a Form Class
+class UserForm(FlaskForm):
+    name = StringField("whats your name", validators=[DataRequired()])
+    user_name = StringField("Whats your username " , validators = [DataRequired()])
+    email = StringField("whats your email", validators=[DataRequired()])
+    favorate_colour = StringField("whats your favourate colour ")
+    password_hash = PasswordField("password" , validators = [DataRequired() , EqualTo('compare_password', message = 'Password must Match!! ')])
+    compare_password = PasswordField('Confirm Password' , validators = [DataRequired()])
+    submit = SubmitField("Submit")
+
+
+
+#Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("whats your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+#create Postes Form
+class PostForm(FlaskForm):
+    title = StringField("title ", validators=[DataRequired()])
+    content = StringField("enter content  ", validators=[DataRequired()] , widget = TextArea())
+    author = StringField("whats the author name ", validators=[DataRequired()])
+    slug = StringField("slug ", validators=[DataRequired()])
+    submit = SubmitField("submit")
+
+
+#crate Password form
+class PasswordForm(FlaskForm):
+    email = StringField("whats your email", validators=[DataRequired()])
+    password_hash = PasswordField("whats your password", validators=[DataRequired()])
+    submit = SubmitField("Submit ")
+#create login Form
+class LoginForm(FlaskForm):
+    user_name =StringField("Username" , validators = [DataRequired()])
+    password = PasswordField("Password" , validators = [DataRequired()])
+    submit = SubmitField("Submitt ")
+
 
 #create a decorator (route)
 
@@ -166,10 +205,10 @@ def delete(id):
 
 
 @app.route('/update/<int:id>', methods = ['GET', 'POST'])
-@login_required
 def update(id):
     form = UserForm()
-    user_to_update = Users.query.get_or_404(id)
+    id = id
+    print(id)
 
     user_to_update = Users.query.get_or_404(id)
     print("somthing happened 2")
---
+++ webforms.py
@@ -0,0 +1,43 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,PasswordField,BooleanField,ValidationError
+from wtforms.validators import DataRequired, EqualTo , Length
+from wtforms.widgets import TextArea
+
+
+
+#Create a Form Class
+class UserForm(FlaskForm):
+    name = StringField("whats your name", validators=[DataRequired()])
+    user_name = StringField("Whats your username " , validators = [DataRequired()])
+    email = StringField("whats your email", validators=[DataRequired()])
+    favorate_colour = StringField("whats your favourate colour ")
+    password_hash = PasswordField("password" , validators = [DataRequired() , EqualTo('compare_password', message = 'Password must Match!! ')])
+    compare_password = PasswordField('Confirm Password' , validators = [DataRequired()])
+    submit = SubmitField("Submit")
+
+
+
+#Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("whats your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+#create Postes Form
+class PostForm(FlaskForm):
+    title = StringField("title ", validators=[DataRequired()])
+    content = StringField("enter content  ", validators=[DataRequired()] , widget = TextArea())
+    author = StringField("whats the author name ", validators=[DataRequired()])
+    slug = StringField("slug ", validators=[DataRequired()])
+    submit = SubmitField("submit")
+
+
+#crate Password form
+class PasswordForm(FlaskForm):
+    email = StringField("whats your email", validators=[DataRequired()])
+    password_hash = PasswordField("whats your password", validators=[DataRequired()])
+    submit = SubmitField("Submit ")
+#create login Form
+class LoginForm(FlaskForm):
+    user_name =StringField("Username" , validators = [DataRequired()])
+    password = PasswordField("Password" , validators = [DataRequired()])
+    submit = SubmitField("Submitt ")
